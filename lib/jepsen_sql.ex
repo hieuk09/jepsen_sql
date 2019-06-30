4b40fa11d744ff54f770823efe1634b747bfc1ce
Hieu Nguyen <hieuk09@gmail.com>
2019-06-30 16:16:56 +0800

    Remove placeholder code

--- lib/jepsen_sql.ex
+++ lib/jepsen_sql.ex
@@ -1,18 +1,2 @@
 defmodule JepsenSql do
-  @moduledoc """
-  Documentation for JepsenSql.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> JepsenSql.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/jepsen_sql_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule JepsenSqlTest do
-  use ExUnit.Case
-  doctest JepsenSql
-
-  test "greets the world" do
-    assert JepsenSql.hello() == :world
-  end
-end
