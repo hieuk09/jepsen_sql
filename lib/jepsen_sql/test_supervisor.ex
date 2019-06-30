1c56e079f2c484c720c15aa13bb94eba22880dcb
Hieu Nguyen <hieuk09@gmail.com>
2019-06-30 16:17:23 +0800

    Use Supervisor for test workers

---
+++ lib/jepsen_sql/test_supervisor.ex
@@ -0,0 +1,20 @@
+defmodule JepsenSql.TestSupervisor do
+  use Supervisor
+  alias JepsenSql.Test
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    children = Enum.map(1..5, fn i ->
+      %{
+        id: "Test#{i}",
+        start: {Test, :start_link, [:work]},
+        restart: :permanent
+      }
+    end)
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
